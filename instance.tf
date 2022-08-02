a177a4990b9c3fc098ccb2b90cff7b0a52a393f1
siarczyn <57362140+siarczyn@users.noreply.github.com>
2022-08-02 05:12:32 +0200

    init commit

---
+++ instance.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+access_key = var.access_key
+secret_key = var.secret_key
+  region = var.aws_region
+}
+
+resource "aws_instance" "example"{
+    ami = "ami-0a1ee2fb28fe05df3"
+    instance_type="t2.nano"
+
+    # #vpc subnet 
+    # subnet_id= "${aws_subnet.main-public-1.id}"
+
+    #security group
+    vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
+
+    # # the public SSH key
+    # key_name = "${aws_key_pair.mykeypair.key_name}"
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "access_key"{
+    type = string
+    default = "AKIATR5XNIFPA2BTG4X4"
+}
+variable "secret_key"{
+    type = string
+    default = "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"
+}
+variable "aws_region"{
+    type = string
+    default = "eu-central-1"
+}
